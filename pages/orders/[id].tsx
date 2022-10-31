1829dc4ed6ed01701e501fb48d4f206ea650add0
Daniel Alanis <dalanis@steplix.com>
2022-10-31 19:59:39 -0300

    se cambia name de api en raiz

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -11,7 +11,7 @@ import { ShopLayout } from '../../components/layouts/ShopLayout';
 import { CartList, OrderSummary } from '../../components/cart';
 import { dbOrders } from '../../database';
 import { IOrder } from '../../interfaces';
-import { tesloApi } from '../../api';
+import { tesloApi } from '../../apiBaseUrl';
 
 
 export type OrderResponseBody = {
